9685036c7ea0ef36ef58a7d1c6c31aede7f600a1
dreas2003 <98417368+dreas2003@users.noreply.github.com>
2022-02-04 20:50:40 -0800

    Add files via upload

---
+++ 2-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <strings.h>
+
+using namespace std;
+int main() {
+   int miles = 4276;
+   int acc = -20;
+   int time = 189000;
+   string num1 = "We have made a long journey of 4276 miles";
+   string num2 = "Our checking account balance is  -20.";
+   string num3 = "About 189000 days ago Columbus stood on this spot.";
+
+   cout << num1 << "\n";
+   cout << num2 << endl;
+   cout << num3 << endl;
+   
+
+    return 0;
+}
